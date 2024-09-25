df4d73ddc1586898c24316d1a5c525c6852a09d7
Douxx <136928935+douxxu@users.noreply.github.com>
2024-09-26 00:09:57 +0200

    Uploaded panel

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random Media</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>RandoMedia</h1>
+        <div id="media-display" class="media-display"></div>
+        <button id="upload-btn">Upload</button>
+        <input type="file" id="file-input" accept="image/*,video/*" style="display: none;">
+    </div>
+    <script src="scripts/script.js"></script>
+    <script src="scripts/moderation.js"></script>
+
+</body>
+</html>
