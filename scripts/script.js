32783cccc2802ed194fa245eac1737a777690bd0
JdM <julien@rmbi.ch>
2024-09-25 21:39:41 +0200

    Refactor media display functionality and file upload

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.json())
             .then(data => {
                 if (data.success) {
+                    localStorage.setItem("mediaPath", data.file);
                     const mediaUrl = mediaPath + data.file;
                     mediaDisplay.innerHTML = `
                         ${data.file.endsWith('.mp4') ? 
