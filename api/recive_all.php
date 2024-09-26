92b2f32ca5eec1ff3f6534bcbb131a3556b7bc31
JdM <julien@rmbi.ch>
2024-09-26 18:01:45 +0200

    Refactor media display functionality to limit the number of files returned

--- api/recive_all.php
+++ api/recive_all.php
@@ -5,5 +5,5 @@ ini_set('display_errors', 1);
 $mediaDir = __DIR__ . '/../medias/';
 $files = array_diff(scandir($mediaDir), ['.', '..']);
 shuffle($files);
-echo json_encode(['success' => true, 'files' => array_values($files)]);
+echo json_encode(['success' => true, 'files' => array_values(array_slice($files, 0, 70))]);
 ?>
